a3b7273e4b5432cea967504f8b65293c5c2e5b71
Narayan Aryal <aryal.coding@gmail.com>
2023-02-12 15:06:49 +1100

    email processing completed

--- src/utils/emails.js
+++ src/utils/emails.js
@@ -3,44 +3,40 @@
 //whom and what to sendemail, make email body
 //use ther previos config to send email
 
+import nodemailer from "nodemailer";
 
-import nodemailer from 'nodemailer'
+const emailProcessing = async (emailInfo) => {
+  try {
+    //config
+    let transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+      },
+    });
 
- const emailProcessing = async (emailInfo) => {
-    try {
-        //config
-        let transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            auth: {
-                user: process.env.SMTP_USER,
-                pass: process.env.SMTP_PASS
-            }
-        });
-
-        //send email
-        const info = await transporter.sendMail(emailInfo)
-        // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
+    //send email
+    const info = await transporter.sendMail(emailInfo);
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //email templeting
 
-export const adminSignUPEmailVerification = ({email, fName}, url) => {
-// send mail with defined transport object
-    let info ={
-        from: `"Register Form" <${email}>`, // sender address
-        to: process.env.SMTP_USER, // list of receivers
-        subject: "New account verification, Action Required", // Subject line
-        text: `Hi ${fName}, Please follow the link ${url} to verify your account`, // plain text body
-        html: `
+export const adminSignUPEmailVerification = ({ email, fName }, url) => {
+  // send mail with defined transport object
+  let info = {
+    from: `"Register Form" <${email}>`, // sender address
+    to: process.env.SMTP_USER, // list of receivers
+    subject: "New account verification, Action Required", // Subject line
+    text: `Hi ${fName}, Please follow the link ${url} to verify your account`, // plain text body
+    html: `
         <p> Hi ${fName}</p>
         </br>
         </br>
@@ -51,14 +47,16 @@ export const adminSignUPEmailVerification = ({email, fName}, url) => {
         </br>
         </br>
         <p>
-        customer care
-
         -------------
+        
+        </br>
+        </br>
+        customer care
+       
         </p>
         
         `, // html body
-        
-      };
+  };
 
-      emailProcessing(info)
-}
+  emailProcessing(info);
+};
