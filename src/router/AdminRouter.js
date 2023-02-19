4f70a534361482f57aa46e0db5341bd1941b0348
Narayan Aryal <aryal.coding@gmail.com>
2023-02-19 12:01:08 +1100

    login admin code completed

--- src/router/AdminRouter.js
+++ src/router/AdminRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createUser, findAdminAndUpdate } from "../model/adminUser/AdminUserModel.js";
-import { v4 as uuidv4 } from 'uuid';
-import { hashPassword } from "../utils/bcrypt.js";
+import {
+  createUser,
+  findAdmin,
+  findAdminAndUpdate,
+} from "../model/adminUser/AdminUserModel.js";
+import { v4 as uuidv4 } from "uuid";
+import { comparePassword, hashPassword } from "../utils/bcrypt.js";
 import { adminSignUPEmailVerification } from "../utils/emails.js";
-
 
 const router = express.Router();
 
@@ -12,31 +15,31 @@ const router = express.Router();
 router.post("/", async (req, res, next) => {
   try {
     // console.log(req.body);
-    req.body.password =  hashPassword(req.body.password)
+    req.body.password = hashPassword(req.body.password);
     req.body.verificationCode = uuidv4();
     const result = await createUser(req.body);
     // console.log(result)
 
+    if (result?._id) {
+      //we need to create a unique url and send email to the client
+      //process for the email
+      const uniqueUrl = `http://localhost:3000/verify?c=${result.verificationCode}&email=${result.email}`;
 
-   if (result?._id) {
-    //we need to create a unique url and send email to the client
-    //process for the email
-    const uniqueUrl = `http://localhost:3000/verify?c=${result.verificationCode}&email=${result.email}`;
+      //call email service
 
-    //call email service
+      adminSignUPEmailVerification(result, uniqueUrl);
 
-    adminSignUPEmailVerification(result, uniqueUrl)
-
-    
-       res.json({
-          status: "success",
-          message: "We have sent an email verification linkto your email. Please check your email and follow your instruction to activate your account",
-        })
-        return }
-       res.json({
-          status: "error",
-          message: "unable to create new admin, please try again later",
-        });
+      res.json({
+        status: "success",
+        message:
+          "We have sent an email verification linkto your email. Please check your email and follow your instruction to activate your account",
+      });
+      return;
+    }
+    res.json({
+      status: "error",
+      message: "unable to create new admin, please try again later",
+    });
   } catch (error) {
     error.errorCode = 500;
     if (error.message.includes("E11000 duplicate key error collection")) {
@@ -50,35 +53,71 @@ router.post("/", async (req, res, next) => {
 
 // email verification
 
-router.post ("/email-verify", async(req, res, next)=> {
+router.post("/email-verify", async (req, res, next) => {
   try {
-    console.log(req.body)
-    const obj ={
+    console.log(req.body);
+    const obj = {
       status: "active",
       verificationCode: "",
-      isEmailVerified: true
+      isEmailVerified: true,
+    };
+    const result = await findAdminAndUpdate(req.body, obj);
+
+    if (result?._id) {
+      res.json({
+        status: "success",
+        message: "Your email is verified, You can login now!",
+      });
+      return;
     }
-const result = await findAdminAndUpdate(req.body, obj)
 
-if(result?._id) { res.json({
-      status:"success",
-      message:"Your email is verified, You can login now!"
-    }) 
-  return}
-    
-    
     res.json({
-      status:"error",
-      message:"Invalid link"
-    })
+      status: "error",
+      message: "Invalid link",
+    });
   } catch (error) {
-    next(error)
-    
+    next(error);
   }
-})
-
-
+});
 
 //admin login
 
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    // check if email exist
+    const result = await findAdmin({ email });
+
+    if (result?.status === "inactive") {
+      return res.json({
+        status: "error",
+        message: "User inactive, check your email and verify your account",
+      });
+    }
+
+    //is password match
+
+    if (result?._id) {
+      const isPasswordMatch = comparePassword(password, result.password);
+
+      if (isPasswordMatch) {
+        result.password = undefined;
+        result.__v = undefined;
+
+        return res.json({
+          status: "success",
+          message: "Login Successful",
+          result,
+        });
+      }
+    }
+
+    res.json({
+      status: "error",
+      message: "Invalid logging details",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 export default router;
--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -5,3 +5,8 @@ const salt = 15
 export const hashPassword = (plainPassword) => {
    return bcrypt.hashSync(plainPassword, salt)
 }
+
+export const comparePassword = (plainPassword, hashPassword) => {
+   return bcrypt.compareSync(plainPassword, hashPassword)
+}
+
